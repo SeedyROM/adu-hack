0c9216ab725f50284dfd4ee113cfd307035e9e20
Zack Kollar <rallokkcaz@gmail.com>
2018-10-20 05:07:09 -0700

    Fixed more bugs and left some nice scaffolding around

--- connections/views.py
+++ connections/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
+from accounts.models import User
 from accounts.models import ContractorInformation
 from connections.models import Service
 
 def search(request):
-    contractors = ContractorInformation.objects.all()
+    contractors = User.objects.filter(user_type=User.CONTRACTOR)
     popular_services = Service.top_5()
 
-    print(popular_services)
+    print(contractors)
 
     return render(request, 'connections/search.html', {
         'services': popular_services,
