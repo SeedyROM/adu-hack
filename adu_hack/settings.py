033a6077fad1073502f09337a91c6953aff00fb3
Zack Kollar <rallokkcaz@gmail.com>
2018-10-19 21:11:33 -0700

    Minor changes to the models

--- adu_hack/settings.py
+++ adu_hack/settings.py
@@ -122,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Authentication
+AUTH_USER_MODEL = 'accounts.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
