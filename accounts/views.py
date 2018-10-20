4db123c38a27cefec338f48798fc9c2cb8bd2b4c
Ian <ianbingham24@gmail.com>
2018-10-19 21:43:08 -0700

    views skeleton

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    pass
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
+def create_acc(request):
+    pass
+
+def update_info(request):
+    pass
+
+def review(request):
+    pass
