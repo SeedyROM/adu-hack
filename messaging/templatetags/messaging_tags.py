bb62364f8ff2584ef60ddba7e35f38b236e1ec25
Chris Jones <chris.charles.jones@gmail.com>
2018-10-20 00:26:33 -0700

    added new message count tag

--- messaging/templatetags/messaging_tags.py
+++ messaging/templatetags/messaging_tags.py
@@ -6,11 +6,11 @@ from django.db.models import Count, Case, When
 
 register = template.Library()
 
+
 @register.simple_tag
 def new_messages_number(user):
-    pass
-    # query_set.aggregate(bool_col=Count(Case(When(my_bool_col=True, then=1))))
-    # chats = Chat.objects.all()
-    # num = 0
-    # for i in chats:
-    #     num += i.messages.all().aggregate(is_read=Count(Case(When(is_read=True, then=1))))
+    chats = Chat.objects.filter(members__in=[user.id])
+    num = 0
+    for i in chats:
+        num += i.messages.all().aggregate(is_read=Count(Case(When(is_read=False, then=1))))['is_read']
+    return num
