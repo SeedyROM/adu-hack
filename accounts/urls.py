186afbb1539b0091bbfbe4f4e455c3cd39c49528
Chelsea Dover <chelseamegandover@gmail.com>
2018-10-20 13:31:38 -0700

    totally real contractor profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'accounts' 
+app_name = 'accounts'
 urlpatterns = [
     path('login', views.login_user, name='login'),
     path('logout', views.log_out, name='logout'),
     path('register_owner', views.register_property_owner_user, name='register_property_owner_user'),
     path('register_contractor', views.register_contractor_user, name='register_contractor_user'),
     path('profile', views.profile, name='profile'),
+    path('smithjones', views.smithjones, name='smithjones'),
 ]
